090be77990cca3080d8bdff12f6cfabf7ff9f1bb
kilfu0701 <kilfu0701@gmail.com>
2014-09-15 15:47:07 +0800

    update debug code and comments.

--- debug/debug.py
+++ debug/debug.py
@@ -1,13 +1,50 @@
 import sys
 
+
+"""
+    @Usage:
+        import Debug as D
+        d = D(level=4, color=True)
+        d.info('Some info', my_vars)
+        d.log('This is Log')
+        d.debug('For debugging')
+        d.error('Fatal Errors', error_objects)
+
+    @param:
+        level: integer.
+        color: boolean.
+
+    @def:
+        info(..)
+        log(..)
+        debug(..)
+        error(..)
+"""
 class Debug(object):
-    def __init__(self, level=3):
+    def __init__(self, level=4, color=False):
+        self.color = color
         self.level = level
         self._pv = {
-            'log': 3,
-            'debug': 2,
-            'error': 1,
+            'info': {
+                'level': 4,
+                'color': ByColors.OKGREEN,
+            },
+            'log': {
+                'level': 3,
+                'color': ByColors.HEADER,
+            },
+            'debug': {
+                'level': 2,
+                'color': ByColors.WARNING,
+            },
+            'error': {
+                'level': 1,
+                'color': ByColors.FAIL,
+            },
         }
+
+    def info(self, msg='', *args, **kwargs):
+        self._pr(sys._getframe().f_code.co_name, msg, *args)
 
     def log(self, msg='', *args, **kwargs):
         self._pr(sys._getframe().f_code.co_name, msg, *args)
@@ -19,7 +56,21 @@ class Debug(object):
         self._pr(sys._getframe().f_code.co_name, msg, *args)
 
     def _pr(self, func, msg, *args):
-        if self.level >= self._pv[func]:
-            print '[{0}] {1} {2}'.format(func.upper(), repr(msg), ' '.join(map(repr, args)))
+        if self.level >= self._pv[func]['level']:
+            if self.color:
+                func_txt = self._pv[func]['color'] + func.upper() + ByColors.ENDC
+            else:
+                func_txt = func.upper()
+
+            print '[{0}] {1} {2}'.format(func_txt, repr(msg), ' '.join(map(repr, args)))
+
+
+class ByColors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
 
 
